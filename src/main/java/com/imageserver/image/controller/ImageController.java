a7cea2ba6593828828f3be87ddf92a1c8d378ab7
qinzhengsheng <535305131@qq.com>
2020-03-24 18:09:06 +0800

    第一次

---
+++ src/main/java/com/imageserver/image/controller/ImageController.java
@@ -0,0 +1,13 @@
+package com.imageserver.image.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ImageController
+ * @2020/3/24
+ */
+@RestController
+@Slf4j
+public class ImageController {
+}
